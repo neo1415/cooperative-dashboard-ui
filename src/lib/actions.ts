0d5568d891dfc6589bba6d0bb26adacd75e45c2e
neo1415 <adedaniel502@gmail.com>
2024-10-06 15:32:12 +0100

    auth finish

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,35 +1,30 @@
-"use server"
+// lib/actions.ts (Server-side)
+import { revalidatePath } from "next/cache";
+import { CooperativeSchema, MemberSchema } from "./formValidationSchemas";
+import prisma from "./prisma";
 
-import { revalidatePath } from "next/cache"
-import { CooperativeSchema, MemberSchema } from "./formValidationSchemas"
-import prisma from "./prisma"
-
-export const submitCcoperativeForm = async (data:CooperativeSchema)=>{
-    // console.log(data + "in the server action")
-    try{
+export const submitCcoperativeForm = async (data: CooperativeSchema) => {
+  try {
     await prisma.cooperative.create({
-        data: {
-...data
+      data: {
+        ...data,
+      },
+    });
+    revalidatePath('/list/cooperatives');
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-        }
-    })
-    revalidatePath('/list/cooperatives')
-    }catch(err){
-        console.log(err)
-    }
-}
-
-export const submitMemberForm = async (data:MemberSchema)=>{
-    // console.log(data + "in the server action")
-    try{
+export const submitMemberForm = async (data: MemberSchema) => {
+  try {
     await prisma.member.create({
-        data: {
-...data
-
-        }
-    })
-    revalidatePath('/list/cooperatives')
-    }catch(err){
-        console.log(err)
-    }
-}
+      data: {
+        ...data,
+      },
+    });
+    revalidatePath('/list/members');
+  } catch (err) {
+    console.error(err);
+  }
+};
